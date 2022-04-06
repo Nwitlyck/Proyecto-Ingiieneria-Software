ec1c19390346c4942d84ea64d8bcac1398e8b230
Dylan Monge Diaz <DylanMongeDiaz@yahoo.com>
2022-04-05 19:00:06 -0600

    ola

--- FormPerfilUsuarios.Designer.vb
+++ FormPerfilUsuarios.Designer.vb
@@ -50,7 +50,7 @@ Partial Class FormPerfilUsuarios
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.PictureBox1.Image = Global.Proyecto_Ingiieneria_Software.My.Resources.Resources.profile_g888983ebd_640
-        Me.PictureBox1.Location = New System.Drawing.Point(153, 96)
+        Me.PictureBox1.Location = New System.Drawing.Point(120, 122)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(129, 134)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -60,10 +60,10 @@ Partial Class FormPerfilUsuarios
         'ButtonCC
         '
         Me.ButtonCC.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.ButtonCC.Font = New System.Drawing.Font("Impact", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ButtonCC.Location = New System.Drawing.Point(12, 622)
+        Me.ButtonCC.Font = New System.Drawing.Font("Bahnschrift Condensed", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCC.Location = New System.Drawing.Point(12, 638)
         Me.ButtonCC.Name = "ButtonCC"
-        Me.ButtonCC.Size = New System.Drawing.Size(129, 72)
+        Me.ButtonCC.Size = New System.Drawing.Size(85, 56)
         Me.ButtonCC.TabIndex = 12
         Me.ButtonCC.Text = "Olvide Contraseña"
         Me.ButtonCC.UseVisualStyleBackColor = True
@@ -71,10 +71,10 @@ Partial Class FormPerfilUsuarios
         'Button1
         '
         Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.Button1.Font = New System.Drawing.Font("Impact", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(825, 622)
+        Me.Button1.Font = New System.Drawing.Font("Bahnschrift Condensed", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(875, 638)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(130, 72)
+        Me.Button1.Size = New System.Drawing.Size(85, 56)
         Me.Button1.TabIndex = 13
         Me.Button1.Text = "Actualizar Informacion"
         Me.Button1.UseVisualStyleBackColor = True
@@ -83,91 +83,91 @@ Partial Class FormPerfilUsuarios
         '
         Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.Label2.Location = New System.Drawing.Point(149, 349)
+        Me.Label2.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(116, 413)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(86, 23)
+        Me.Label2.Size = New System.Drawing.Size(133, 23)
         Me.Label2.TabIndex = 15
-        Me.Label2.Text = "Direccion"
+        Me.Label2.Text = "Direccion (opcional)"
         '
         'TextBoxUbicacion
         '
         Me.TextBoxUbicacion.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.TextBoxUbicacion.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.TextBoxUbicacion.Location = New System.Drawing.Point(153, 386)
+        Me.TextBoxUbicacion.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxUbicacion.Location = New System.Drawing.Point(120, 449)
         Me.TextBoxUbicacion.Multiline = True
         Me.TextBoxUbicacion.Name = "TextBoxUbicacion"
-        Me.TextBoxUbicacion.Size = New System.Drawing.Size(320, 141)
+        Me.TextBoxUbicacion.Size = New System.Drawing.Size(285, 141)
         Me.TextBoxUbicacion.TabIndex = 14
         '
         'Label1
         '
         Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.Label1.Location = New System.Drawing.Point(657, 121)
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(501, 121)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(72, 23)
+        Me.Label1.Size = New System.Drawing.Size(138, 23)
         Me.Label1.TabIndex = 17
-        Me.Label1.Text = "Nombre"
+        Me.Label1.Text = "Nombre (obligatorio)"
         '
         'TextBoxNombre
         '
         Me.TextBoxNombre.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.TextBoxNombre.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.TextBoxNombre.Location = New System.Drawing.Point(655, 147)
+        Me.TextBoxNombre.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxNombre.Location = New System.Drawing.Point(505, 147)
         Me.TextBoxNombre.Name = "TextBoxNombre"
-        Me.TextBoxNombre.Size = New System.Drawing.Size(133, 31)
+        Me.TextBoxNombre.Size = New System.Drawing.Size(190, 30)
         Me.TextBoxNombre.TabIndex = 16
         '
         'Label3
         '
         Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.Label3.Location = New System.Drawing.Point(658, 206)
+        Me.Label3.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(501, 230)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(131, 23)
+        Me.Label3.Size = New System.Drawing.Size(187, 23)
         Me.Label3.TabIndex = 19
-        Me.Label3.Text = "Primer Apellido"
+        Me.Label3.Text = "Primer Apellido (obligatorio)"
         '
         'TextBox1apellido
         '
         Me.TextBox1apellido.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.TextBox1apellido.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.TextBox1apellido.Location = New System.Drawing.Point(655, 232)
+        Me.TextBox1apellido.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1apellido.Location = New System.Drawing.Point(505, 256)
         Me.TextBox1apellido.Name = "TextBox1apellido"
-        Me.TextBox1apellido.Size = New System.Drawing.Size(133, 31)
+        Me.TextBox1apellido.Size = New System.Drawing.Size(190, 30)
         Me.TextBox1apellido.TabIndex = 18
         '
         'Label4
         '
         Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.Label4.Location = New System.Drawing.Point(652, 283)
+        Me.Label4.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(501, 339)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(147, 23)
+        Me.Label4.Size = New System.Drawing.Size(183, 23)
         Me.Label4.TabIndex = 21
-        Me.Label4.Text = "Segundo Apellido"
+        Me.Label4.Text = "Segundo Apellido (opcional)"
         '
         'TextBox2apellido
         '
         Me.TextBox2apellido.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.TextBox2apellido.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.TextBox2apellido.Location = New System.Drawing.Point(656, 309)
+        Me.TextBox2apellido.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2apellido.Location = New System.Drawing.Point(505, 365)
         Me.TextBox2apellido.Name = "TextBox2apellido"
-        Me.TextBox2apellido.Size = New System.Drawing.Size(133, 31)
+        Me.TextBox2apellido.Size = New System.Drawing.Size(190, 30)
         Me.TextBox2apellido.TabIndex = 20
         '
         'User
         '
         Me.User.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.User.AutoSize = True
-        Me.User.Font = New System.Drawing.Font("Impact", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.User.Location = New System.Drawing.Point(149, 232)
+        Me.User.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.User.Location = New System.Drawing.Point(169, 263)
         Me.User.Name = "User"
-        Me.User.Size = New System.Drawing.Size(46, 23)
+        Me.User.Size = New System.Drawing.Size(38, 23)
         Me.User.TabIndex = 23
         Me.User.Text = "User"
         '
@@ -175,20 +175,20 @@ Partial Class FormPerfilUsuarios
         '
         Me.Label6.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.Label6.Location = New System.Drawing.Point(288, 96)
+        Me.Label6.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(501, 534)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(48, 23)
+        Me.Label6.Size = New System.Drawing.Size(122, 23)
         Me.Label6.TabIndex = 24
-        Me.Label6.Text = "Edad"
+        Me.Label6.Text = "Edad (obligatoria)"
         '
         'NumericUpDownEdad
         '
         Me.NumericUpDownEdad.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.NumericUpDownEdad.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.NumericUpDownEdad.Location = New System.Drawing.Point(315, 122)
+        Me.NumericUpDownEdad.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NumericUpDownEdad.Location = New System.Drawing.Point(505, 559)
         Me.NumericUpDownEdad.Name = "NumericUpDownEdad"
-        Me.NumericUpDownEdad.Size = New System.Drawing.Size(45, 31)
+        Me.NumericUpDownEdad.Size = New System.Drawing.Size(45, 30)
         Me.NumericUpDownEdad.TabIndex = 28
         Me.NumericUpDownEdad.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
@@ -196,26 +196,26 @@ Partial Class FormPerfilUsuarios
         '
         Me.Label7.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.Label7.Location = New System.Drawing.Point(657, 360)
+        Me.Label7.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(501, 448)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(68, 23)
+        Me.Label7.Size = New System.Drawing.Size(120, 23)
         Me.Label7.TabIndex = 30
-        Me.Label7.Text = "Celular"
+        Me.Label7.Text = "Celular (opcional)"
         '
         'TextBoxCelular
         '
         Me.TextBoxCelular.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.TextBoxCelular.Font = New System.Drawing.Font("Impact", 14.25!)
-        Me.TextBoxCelular.Location = New System.Drawing.Point(656, 386)
+        Me.TextBoxCelular.Font = New System.Drawing.Font("Bahnschrift Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxCelular.Location = New System.Drawing.Point(505, 474)
         Me.TextBoxCelular.Name = "TextBoxCelular"
-        Me.TextBoxCelular.Size = New System.Drawing.Size(133, 31)
+        Me.TextBoxCelular.Size = New System.Drawing.Size(190, 30)
         Me.TextBoxCelular.TabIndex = 29
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(633, 579)
+        Me.DataGridView1.Location = New System.Drawing.Point(822, 0)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(151, 115)
         Me.DataGridView1.TabIndex = 31
